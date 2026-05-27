99b6a573a47addbd1453a71eef724d52005163ed
danylenko-14-dbtu <danylenko.14@btu.kharkov.ua>
2026-05-27 15:19:23 +0300

    fix: telegram OG - permanentRedirect => redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-import { permanentRedirect } from "next/navigation";
+import { redirect } from "next/navigation";
 import { defaultLocale } from "@/lib/i18n";
 
 export default function RootPage() {
-  permanentRedirect(`/${defaultLocale}`);
+  redirect(`/${defaultLocale}`);
 }
